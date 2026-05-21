a391495a036c0ec3e1ad5ed1d58278e849c99b47
Ori Haggai <orihagai@proton.me>
2026-05-21 12:36:33 -0400

    Update nix.nix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -9,7 +9,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 30d";
+    options = "--delete-older-than 60d";
   };
 
   nixpkgs.config.allowUnfree = true;
